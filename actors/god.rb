f48cedbfbbce5bc62e0c69325cbdd9fafe289d0c
Ezra Zygmuntowicz <ez@engineyard.com>
2009-02-16 18:51:58 -0800

    god actor by raggi

---
+++ actors/god.rb
@@ -0,0 +1,46 @@
+# from a pastie by raggi
+require 'drb'
+
+class God < Nanite::Actor
+  expose :status, :stop, :start, :restart
+
+  def initialize
+    @socket = "drbunix:///tmp/god.17165.sock"
+    reconnect
+  end
+
+  def reconnect
+    begin; DRb.stop_service; rescue Exception; end
+    @god = nil
+    DRb.start_service("druby://127.0.0.1:0")
+    @god = DRbObject.new(nil, @socket)
+  end
+
+  def god(&blk)
+    once ||= false
+    @god.instance_eval(&blk)
+  rescue DRb::DRbConnError => e
+    reconnect
+    unless once == true
+      once = true
+      retry
+    end
+    raise e
+  end
+
+  def status payload
+    god { status }
+  end
+
+  def stop payload
+    god { control(payload, 'stop') }
+  end
+
+  def start payload
+    god { control(payload, 'start') }
+  end
+
+  def restart payload
+    god { control(payload, 'restart') }
+  end
+end
--- init.rb
+++ init.rb
@@ -2,3 +2,4 @@ register Rabbit.new
 register Monit.new
 register Initd.new
 register Deployer.new
+#register God.new
